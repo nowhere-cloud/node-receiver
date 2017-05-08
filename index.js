7911ed4e44c763c48084151b9b8bea4dccf20b62
Lawrence Law <d201.c317@gmail.com>
2017-05-08 10:51:56 +0100

    Somplify the design, hope it helps

--- index.js
+++ index.js
@@ -5,55 +5,25 @@
 const AMQP = require('amqplib');
 const Mongo = require('./models-mongo/');
 
-/**
- * AMQP Access
- *
- * @class Rabbit
- */
-class Rabbit {
-  /**
-   * Creates an instance of Rabbit.
-   *
-   * @returns {void}
-   * @memberOf Rabbit
-   */
-  constructor() {
-    this.AURI = process.env.AMQP_URI || 'amqp://localhost';
-    this.listen = 'out';
-  }
-
-  /**
-   * message receiver
-   * Workflow:
-   *  1. Connect to RabbitMQ
-   *  2. Parse Message
-   *  3.
-   * @returns {void}
-   * @memberOf Rabbit
-   */
-  receiveMessage() {
-    const listen = this.listen;
-    AMQP.connect(this.AURI).then((conn) => {
-      process.once('SIGINT', () => {
-        conn.close();
-      });
-      return conn.createChannel().then((ch) => {
-        ch.assertQueue(listen).then(() => {
-          ch.consume(listen, (msg) => {
-            let msgContent = JSON.parse(msg.content.toString());
-            return Mongo.Task.update({
-              uuid: msg.properties.correlationId.toString()
-            }, {
-              result: msgContent
-            });
-          }, {
-            noAck: true
-          });
+const listen = 'out';
+AMQP.connect(process.env.AMQP_URI).then((conn) => {
+  process.once('SIGINT', () => {
+    conn.close();
+  });
+  return conn.createChannel().then((ch) => {
+    ch.assertQueue(listen).then(() => {
+      ch.consume(listen, (msg) => {
+        return Mongo.Task.update({
+          uuid: msg.properties.correlationId.toString()
+        }, {
+          result: JSON.parse(msg.content.toString())
         });
+      }, {noAck: true})
+      .then((rsvp) => {
+        console.log(rsvp);
       });
-    }).catch(console.warn);
-  }
-}
-
-let Worker = new Rabbit();
-Worker.receiveMessage();
+    });
+  });
+}).catch((e) => {
+  console.warn(e);
+});
