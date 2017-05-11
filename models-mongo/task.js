9dee0368a63d0394a881ac71801c77f616ab9609
Lawrence Law <d201.c317@gmail.com>
2017-05-12 00:12:42 +0100

    Use Mixed Instead

--- models-mongo/task.js
+++ models-mongo/task.js
@@ -16,7 +16,7 @@ const vm_task = new Schema({
   uuid: String,
   sent: Boolean,
   user: Number,
-  result: {}
+  result: Schema.Types.Mixed
 }, {
   timestamps: true,
   minimize: false // To Save Empty Objects!!!
